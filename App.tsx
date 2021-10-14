b647c8180be0528602524850dcccef4017fb2dc1
luizGonzaga29 <luizgonzagacabral29@gmail.comgit config --global user.email luizgonzagacabral29@gmail.com>
2021-10-13 23:22:51 -0300

    Ex 02

--- App.tsx
+++ App.tsx
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useDebugValue, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground,TextInput,Button,Image } from 'react-native';
 
 export default function App() {
+
+  const [texto, setTexto] = useState("");
+  const [text, setText] = useState("");
+ 
   return (
     <View style={styles.container}>
-      <Text>Primeiro app React Native</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor = "#bac6d4"/>
+
+      <View style={styles.container1}>
+       <Text style={styles.headerText} 
+       >{text}</Text>
+       </View>
+
+      <ImageBackground  
+      source={require('./images/space.png')} 
+      style={styles.img}  
+      >
+        <Image style={styles.profile} source={require('./images/cat.png')}/>
+        
+        <TextInput 
+        style={styles.input}
+        placeholder="Digite um título"
+        placeholderTextColor='#cfbcbd'
+        onChangeText={(text) => setTexto(text)}
+        
+        />
+
+        <Button 
+        title="Exibir título"
+        onPress={() => setText(texto)}
+        
+        />
+      </ImageBackground>
     </View>
   );
 }
@@ -14,8 +44,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  img:{
+    flex:1,
+    justifyContent:'center',
+    resizeMode:'cover',
+    alignItems:'center'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderColor:'#cfbcbd',
+    width:'50%',
+    paddingLeft:10,
+    color:'#cfbcbd'
+  },
+  txt:{
+    color:'#cfbcbd',
+    
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight:'bold',
+    color: '#cfbcbd'
+  },
+
+  container1: {
+    paddingTop: 30,
+    paddingBottom: 20,
+    backgroundColor: 'navy',
+    alignItems: 'center',
+    height:'10%'
+  },
+  profile:{
+    height: 80,
+    width: 80,
+    borderRadius: 120/2,
+    
+  },
+  
+  
 });
